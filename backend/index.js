9e9a90553e6e0efde2364f425d9c07e7fa26f935
HanifFathan10 <haniffathan33@gmail.com>
2023-09-24 17:34:17 +0700

    menambahkan credentials

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true)
   next();
 });
 
