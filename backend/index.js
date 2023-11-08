325004e4a986a7940b31f7021baf66a4fdc7e6f8
Haniep Fathan Riziq <haniffathan33@gmail.com>
2023-11-08 14:35:45 +0700

    fix bug

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, "http://localhost:5000/auth/google/callback");
+const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, "https://cini-kupi-react-js.vercel.app/auth/google/callback");
 
 const scopes = ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"];
 
