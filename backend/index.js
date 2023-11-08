93ac1f6cb26d006d269b060b5a160e0e6cd1308a
Haniep Fathan Riziq <haniffathan33@gmail.com>
2023-11-08 14:33:10 +0700

    fix bug

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, "https://cini-kupi-react-js-api.vercel.app/auth/google/callback");
+const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, "http://localhost:5000/auth/google/callback");
 
 const scopes = ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"];
 
@@ -73,23 +73,9 @@ app.get("/auth/google/callback", async (req, res) => {
   const payload = {
     _id: user._id,
     username: user.username,
-    email: user.email,
-    refresh_token: user.refresh_token,
+    email: user.email
   };
-  const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 60 * 60 * 2 });
-  const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: 60 * 60 * 1,
-  });
-
-  await usersConnection.findOneAndUpdate(
-    { _id: user._id }, // Kriteria pencarian
-    { refresh_token: refreshToken } // Nilai yang akan diupdate
-  );
-
-  res.cookie("refreshToken", refreshToken, {
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1,
-  });
+  const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '60s' });
 
   return res.redirect(`https://cini-kupi.vercel.app/auth-success?accessToken=${accessToken}`);
 });
